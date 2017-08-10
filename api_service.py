1782456cb1cb85e9260744e1c3b6d5e3f1a6311b
Samir Pujari <samir@Samirs-MacBook-Pro.local>
2017-08-09 20:44:08 -0700

    Putting in code for api_service to return data

--- api_service.py
+++ api_service.py
@@ -7,11 +7,28 @@ import utils
 def execute(msg_dict):
     '''
     This is the function that is executed for the api query parameters.
+    TODO: Clean up variable names and code quality.
     '''
     conns = utils.ApplicationConnections()
     db = conns.get_db()
-    #db.all_tweets.find()
-    pass
+
+    tmp_lat = float(msg_dict['lat'])
+    tmp_long = float(msg_dict['long']) 
+    tmp_rad = float(msg_dict['radius'])
+    tmp_amt = int(msg_dict['amount'])
+    tmp_search = msg_dict['search']
+    if not tmp_search:
+        cur = db.all_tweets.find({'geo': {'$within': {'$center': [[tmp_lat, tmp_long], tmp_rad]}}},
+                                 {'_id': 0}).sort([('timestamp_ms', -1)]).limit(tmp_amt)
+    else:
+        '''
+        TODO: Look into using the $text $search functionality once a text index is added.
+        '''
+        cur = db.all_tweets.find({'text': {'$regex': tmp_search},
+                                  'geo': {'$within': {'$center': [[tmp_lat, tmp_long], tmp_rad]}}},
+                                 {'_id': 0}).sort([('timestamp_ms', -1)]).limit(tmp_amt)
+    out = [x for x in cur]
+    return out
 
 if __name__ == '__main__':
     msg_dict = {}
@@ -35,4 +52,4 @@ if __name__ == '__main__':
     msg_dict['radius'] = args.radius
     msg_dict['search'] = args.search
 
-    execute(msg_dict)
+    print execute(msg_dict)
